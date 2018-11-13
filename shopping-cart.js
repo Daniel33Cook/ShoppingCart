99d804afb434c298f07bb0958260de1a58ead3f8
Daniel33Cook <daniel33.cook@gmail.com>
2018-11-13 21:09:52 +0000

    Basic add to cart working.

--- shopping-cart.js
+++ shopping-cart.js
@@ -15,6 +15,13 @@ function ShoppingCart() {
     name: 'Beans',
     price: '4'
   }]);
+
+
+  this.cartItems = ko.observableArray();
+
+  this.addToBasket = function(item) {
+    this.cartItems.push(item);
+  }.bind(this);
 };
 
 ko.applyBindings(new ShoppingCart());
