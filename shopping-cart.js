fc9b397bb2903d7e569a735181ce59e1771f8423
Daniel33Cook <daniel33.cook@gmail.com>
2018-11-13 21:03:14 +0000

    Rendering of items.

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,5 +1,20 @@
-function ShoppingCart() {
 
+
+function ShoppingCart() {
+  // TODO: fetch items from server.
+  this.items = ko.observableArray([{
+    name: 'Peas',
+    price: '1'
+  }, {
+    name: 'Eggs',
+    price: '2'
+  }, {
+    name: 'Milk',
+    price: '3'
+  }, {
+    name: 'Beans',
+    price: '4'
+  }]);
 };
 
 ko.applyBindings(new ShoppingCart());
