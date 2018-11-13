965999a160f4309b7fe6fe4326549a859cdaa0df
Daniel33Cook <daniel33.cook@gmail.com>
2018-11-13 23:15:05 +0000

    Currency conversion working.

--- shopping-cart.js
+++ shopping-cart.js
@@ -2,23 +2,30 @@ function ShoppingCart() {
   // TODO: fetch items from server.
 
   // prices are in GBP.
-  this.items = ko.observableArray([{
+
+  var itemData = [{
     id: 1,
     name: 'Peas',
-    price: '0.95'
+    basePrice: ko.observable('0.95'),
+    price: ko.observable('0.95')
   }, {
     id: 2,
     name: 'Eggs',
-    price: '2.10'
+    basePrice: ko.observable('2.10'),
+    price: ko.observable('2.10')
   }, {
     id: 3,
     name: 'Milk',
-    price: '1.30'
+    basePrice: ko.observable('1.30'),
+    price: ko.observable('1.30')
   }, {
     id: 4,
     name: 'Beans',
-    price: '0.73'
-  }]);
+    basePrice: ko.observable('0.73'),
+    price: ko.observable('0.73')
+  }];
+
+  this.items = ko.observableArray(itemData);
 
   //TODO: fetch from server.
   this.currencyMapping = {
@@ -31,6 +38,23 @@ function ShoppingCart() {
 
   this.currency.subscribe(function(newValue) {
       this.currencySymbol(this.currencyMapping[newValue]);
+
+      $.ajax({
+        url : 'http://apilayer.net/api/live?access_key=f4f8f80bf2bf594b04dfc41aa0b85ee4&currencies=' + newValue + '&source=GBP&format=1',
+        success: function(response) {
+
+          if (response.quotes) {
+            var currencyRate = response.quotes['GBP' + newValue];
+            if (typeof currencyRate !== 'undefined') {
+              for (key in this.items()) {
+                var item = this.items()[key];
+                item['price'](item['basePrice']() * currencyRate);
+              }
+              this.calculateTotalPrice();
+            }
+          }
+        }.bind(this)
+      });
   }.bind(this));
 
   this.currencySymbol = ko.observable(this.currencyMapping[this.currency()]);
@@ -40,9 +64,8 @@ function ShoppingCart() {
   this.cartItems = ko.observableArray();
 
   // General function for formatting of prices
-
   this.formatPrice = function(price) {
-      return this.currencySymbol() + price;
+      return this.currencySymbol() + Math.round(price * 100) / 100;
   };
 
   // Add an item to basket.
@@ -67,26 +90,21 @@ function ShoppingCart() {
   }.bind(this);
 
   // Calculate total price
-  this.totalQuantity.subscribe(function() {
+  this.calculateTotalPrice = function() {
     var total = 0;
 
     var cartItems = this.cartItems();
 
-    console.log(cartItems);
-
     for (key in cartItems) {
       var cartItem = cartItems[key];
-
-
-      console.log(cartItem.price);
-
-      console.log(cartItem.qty());
-
-      total += (cartItem.price * cartItem.qty());
+      total += (cartItem.price() * cartItem.qty());
     }
 
     this.totalPrice(total);
-  }.bind(this));
+  };
+
+  this.totalQuantity.subscribe(this.calculateTotalPrice.bind(this));
+
 
   // Currency conversion via api
 
